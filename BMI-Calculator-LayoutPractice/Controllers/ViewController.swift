99c75ebed22c19d7b4b507eca059d65b87248503
CyrilusK <kardash199696@mail.ru>
2022-06-13 17:02:11 +0300

    added project

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -1,20 +1,22 @@
-//
-//  ViewController.swift
-//  BMI-Calculator-LayoutPractice
-//
-//  Created by Angela Yu on 21/08/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func choicedHeight(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + " м"
+    }
+    
+    @IBAction func choicedWeight(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0f", sender.value) + " кг"
+    }
+    
 }
 
