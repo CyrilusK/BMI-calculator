62a99491869d4bdba40875925153e2b30b0afcfe
CyrilusK <kardash199696@mail.ru>
2022-06-14 23:44:41 +0300

    added secondviewcontroller and bmi

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightValue: UISlider!
+    @IBOutlet weak var weightValue: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,13 @@ class ViewController: UIViewController {
     
     @IBAction func choicedWeight(_ sender: UISlider) {
         weightLabel.text = String(format: "%.0f", sender.value) + " кг"
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let height = heightValue.value
+        let weight = weightValue.value
+        let bmi = weight / (height * height)
+        print(bmi)
     }
     
 }
